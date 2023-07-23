ec6ea017917d11aea86a0d19f68f41cd0c1952b8
Vinicius Barbosa Dos Santos <62946143+vini-barbo@users.noreply.github.com>
2023-07-23 14:53:12 -0300

    Reorganization. Now I want a slideshow

--- Myport/src/App.vue
+++ Myport/src/App.vue
@@ -22,9 +22,7 @@ export default{
 
 <template>
   <main>
-    <meet_barbosa sectionStyleWidth="80vw" sectionStyleHeight="80vh"></meet_barbosa>
-    <p> the Tech I know</p>
-    <meet_tecno sectionStyleWidth="80vw" sectionStyleHeight="30vh"></meet_tecno>
+    <meet_barbosa ></meet_barbosa>
   </main>
 </template>
 
@@ -38,26 +36,6 @@ export default{
   margin: 0;
   padding: 0;
   border: 0;
-  display: block;
 }
 
-
-main{
-  width: 98vw;
-  border: 0px solid black;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  align-content: center;
-  overflow: hidden;
-  margin-top: 40px;
-  padding-bottom: 40px;
-}
-
-p{
-  font-size: 4rem;
-  text-transform: uppercase;
-  margin-top: 30px;
-  font-weight: bold;
-}
 </style>
--- Myport/src/components/apple_section/MeetBarbosa.vue
+++ Myport/src/components/apple_section/MeetBarbosa.vue
@@ -1,27 +1,22 @@
 <script>
     export default{
-        props:['sectionStyleWidth','sectionStyleHeight'],
         name:'meet_barbosa'
     }
 </script>
 
 <template>
-    <section :style="{ 'width':sectionStyleWidth, 'height':sectionStyleHeight}">
+    <section>
     <main>
         <div class="text">
             <h1>MEET BARBOSA</h1>
-            <h2>Front-end developer</h2>
+            <h2>Front-end developer for smart companies</h2>
         </div>
-        <div class="icons">
-            <span>
-                <button class="fab fa-github"></button>
-            </span>
-            <span>
-                <button class="fa-brands fa-linkedin"></button>
-            </span>
-            <span>
-                <button class="fa fa-envelope" aria-hidden="true"></button>
-            </span>
+        <div class="contact">
+            <div class="icons">
+                <button  href="/" target="_blank"><i class="fab fa-github"></i></button>
+                <button  href="/" target="_blank"><i class="fa-brands fa-linkedin"></i></button>
+                <button  href="/" target="_blank" > <i class="fa fa-envelope" aria-hidden="true"></i></button>
+            </div>
         </div>
     </main>
     </section>
@@ -29,14 +24,18 @@
 </template>
 
 <style scoped>
-
+/*Stylization of the main part*/
 section{
-    text-align: center;
+    
+    width: 90vw;
+    height: 90vh;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
     background-color: #eceff1;
     border-radius: 20px;
-    margin-top: 40px;
-    transition: 0.5s; 
-    transition-timing-function: ease;
+    text-align: center;
   }
   
 section:hover{
@@ -47,30 +46,46 @@ section:hover{
 main{
     width: 100%;
     height: 100%;
-    display: flex;
+    display: grid;
+    grid-template-columns: 2fr 1fr ;
+    gap: 0;
     
-    align-content: center;
-    align-items: center;
-    flex-direction: column;
 }
 
-main div{
+/*text section*/
+
+h1,h2{
+    text-align: start;
+    margin:0;
+   margin-left: 2%; 
+}
+
+h1{
+    font-size: 7rem;
+    width: 80%;
+}
+
+h2{
     font-size: 2rem;
+    color: #a1a1a1;
+    width: 80%;
 }
 
-div.text{
-    margin-bottom: 20%;
-}
 
-div.icons{
-    width: 60%;
-    font-size: 6rem;
+/*icons section*/
+
+div.icons {
     display: flex;
-    justify-content: space-around;
-
+    justify-content: space-between; 
 }
 
-span{
+
+button{
+    font-size: 4rem;
+    background-color: transparent;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
 }
 
 </style>
