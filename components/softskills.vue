cea3336548ba06c0be87e24d5cf31b8e70cb7c09
Vinicius Barbosa Dos Santos <62946143+vini-barbo@users.noreply.github.com>
2024-03-28 11:31:21 -0300

    add = icons to the softskill section

--- components/softskills.vue
+++ components/softskills.vue
@@ -1,15 +1,58 @@
+<script setup lang="ts">
+const yearsOfExperience = ref(Math.abs(new Date().getFullYear() - 2023));
+</script>
+
 <template>
   <UIContentArea>
-    <section class="w-full flex flex-col gap-4 md:gap-8 ">
+    <section class="w-full flex flex-col gap-4 md:gap-8">
       <h2 class="subtitle">My skills</h2>
       <h1 class="title">Expertise</h1>
       <aside class="grid md:grid-cols-4 gap-6">
-        <div class="card" v-for="c in 4">
+        <div class="card">
           <div class="cardImage">
             <img src="/bachelor.svg" alt="" />
           </div>
           <div class="cardText">
             <h3>Experience as a Teacher</h3>
+            <h4>
+              I bring valuable teaching experience to the table, enabling me to
+              explain complex technical concepts clearly and supportively to
+              diverse audiences.
+            </h4>
+          </div>
+        </div>
+        <div class="card">
+          <div class="cardImage">
+            <img src="/open-mind.svg" alt="" />
+          </div>
+          <div class="cardText">
+            <h3>Open to learn new subjects</h3>
+            <h4>
+              I bring valuable teaching experience to the table, enabling me to
+              explain complex technical concepts clearly and supportively to
+              diverse audiences.
+            </h4>
+          </div>
+        </div>
+        <div class="card">
+          <div class="cardImage">
+            <img src="/handshake.svg" alt="" />
+          </div>
+          <div class="cardText">
+            <h3>Strong Teamwork Mindset</h3>
+            <h4>
+              I bring valuable teaching experience to the table, enabling me to
+              explain complex technical concepts clearly and supportively to
+              diverse audiences.
+            </h4>
+          </div>
+        </div>
+        <div class="card">
+          <div class="cardImage">
+            <img src="/workfromhome.svg" alt="" />
+          </div>
+          <div class="cardText">
+            <h3>Openness to Work Remotely</h3>
             <h4>
               I bring valuable teaching experience to the table, enabling me to
               explain complex technical concepts clearly and supportively to
@@ -32,7 +75,7 @@
 }
 
 .cardImage {
-  @apply h-20 w-fit aspect-square bg-white rounded-md p-4;
+  @apply flex justify-center items-center h-20 w-fit aspect-square bg-white rounded-md p-4;
 }
 
 .cardText {
