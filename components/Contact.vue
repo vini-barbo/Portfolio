43ddf38e8efd8f9bd6dd12467a20d9520e53208d
Vinicius Barbosa Dos Santos <62946143+vini-barbo@users.noreply.github.com>
2024-04-10 21:02:33 -0300

    Finished all the site

--- components/Contact.vue
+++ components/Contact.vue
@@ -24,98 +24,43 @@ const copyToClipboard = async () => {
       <h2 class="subtitle text-center md:ml-1 md:text-left md:mb-4">
         Get in touch
       </h2>
-      <h1 class="title md:my-5 text-center md:text-left">View my socials</h1>
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 gap-y-5">
-        <aside class="flex flex-col gap-8 justify-between md:p-3 !hidden">
-          <div class="grid grid-cols-2 gap-5 text-center">
-            <div>
-              <div>Linkedin</div>
-              <div class="h-20 md:h-32">
-                <a>
-                  <img src="/linkedin-icon-2.svg" alt="Linkedin link" />
-                </a>
-              </div>
-            </div>
-            <div>
-              <div>Github</div>
-              <div class="h-20 md:h-32">
-                <a href="">
-                  <img src="/github.svg" alt="" />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div class="md:text-xl">Mail me:</div>
-            <div class="flex justify-between">
-              <input
-                class="border p-3 px-5 rounded-md md:text-2xl"
-                value="vinicius25bsantos@gmail.com"
-                style="
-                  border-top-right-radius: 0px;
-                  border-bottom-right-radius: 0px;
-                "
-              />
-              <div
-                class="border-main-mark3 border p-3 rounded-md flex items-center focus:outline-0 md:text-2xl"
-                style="
-                  border-top-left-radius: 0px;
-                  border-bottom-left-radius: 0px;
-                "
-              >
-                <button @click="copyToClipboard">Copy</button>
-              </div>
-            </div>
-          </div>
-        </aside>
-        <aside class="2xl:max-h-[40vw] 2xl:aspect-square md:p-3">
-          <img
-            class="rounded-md shadow-lg md:h-[100%] hover:shadow-2xl hover:scale-105 transition-all"
-            src="/linkedin-profile.png"
-            alt=""
-          />
-        </aside>
-        <aside
-          class="flex flex-col 2xl:max-h-[40vw] 2xl:aspect-square md:p-3 justify-between"
-        >
-          <img
-            class="rounded-md shadow-lg md:h-[75%] hover:shadow-2xl hover:scale-105 transition-all"
-            src="/git-profile.jpg"
-            alt=""
-          />
+      <h1 class="title md:my-5 text-center md:text-left">Contact me</h1>
 
-          <!-- <div class="group w-full">
-            <UIButton text="Copy my email" :size="'md'" :icon="true" class="group-hover:stroke-main-mark2" >
-              <svg class="h-14" fill="#ffff" height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 75.294 75.294" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M66.097,12.089h-56.9C4.126,12.089,0,16.215,0,21.286v32.722c0,5.071,4.126,9.197,9.197,9.197h56.9 c5.071,0,9.197-4.126,9.197-9.197V21.287C75.295,16.215,71.169,12.089,66.097,12.089z M61.603,18.089L37.647,33.523L13.691,18.089 H61.603z M66.097,57.206h-56.9C7.434,57.206,6,55.771,6,54.009V21.457l29.796,19.16c0.04,0.025,0.083,0.042,0.124,0.065 c0.043,0.024,0.087,0.047,0.131,0.069c0.231,0.119,0.469,0.215,0.712,0.278c0.025,0.007,0.05,0.01,0.075,0.016 c0.267,0.063,0.537,0.102,0.807,0.102c0.001,0,0.002,0,0.002,0c0.002,0,0.003,0,0.004,0c0.27,0,0.54-0.038,0.807-0.102 c0.025-0.006,0.05-0.009,0.075-0.016c0.243-0.063,0.48-0.159,0.712-0.278c0.044-0.022,0.088-0.045,0.131-0.069 c0.041-0.023,0.084-0.04,0.124-0.065l29.796-19.16v32.551C69.295,55.771,67.86,57.206,66.097,57.206z"></path> 
-              </g> 
-              </g>
-              </svg>
-              
-            </UIButton>
-          </div> -->
+      <aside class="grid md:grid-cols-2 gap-7 2xl:max-h-[40vw] md:p-1">
+        <a href="https://www.linkedin.com/in/vinicius-barbosa-8204604b/" target="_blank" class="imgPlaceholder"
+          style="  background-image: url('/linkedin-profile.png'); ">
+          <img class="imgPlaceholderIcon" src="/linkedin-icon-2.svg" alt="">
+        </a>
+
+        <a href="https://github.com/vini-barbo" target="_blank" class="imgPlaceholder"
+          style="  background-image: url('/git-profile.jpg');">
+          <img class="imgPlaceholderIcon" src="/github.svg" alt="">
+        </a>
+      </aside>
+
+      <div class="grid md:grid-cols-2 gap-7 2xl:max-h-[40vw] md:p-1">
+        <aside class="card " @click="
+          copyToClipboard()
+          ">
+          <img class="cardImage" src="/email.svg" alt="Linkedin link" />
+          <span class="cardText text-[90%]">vinicius2508@hotmail.com</span>
+          <img class="cardImage " src="/copy.svg" alt="Linkedin link" />
         </aside>
-        <aside v-for="c in 2" class="card">
-          <img
-            class="aspect-square h-10"
-            src="/linkedin-icon-2.svg"
-            alt="Linkedin link"
-          />
-          <div class="cardText">
-            <div>
-              <span>Email</span>
-              <span>vinicius2508@hotmail.com</span>
-            </div>
-            <span>Clique para copiar -></span>
-          </div>
-        </aside>
+
+        <a class="card" href="https://wa.me//5581993531750?text=Vi%20o%20seu%20perfil%20e%20gostaria%20de%20conversar."
+          target="_blank">
+          <img class="cardImage" src="/whatsapp.svg" alt="Linkedin link" />
+          <span class="cardText">+55 (81) 9 93531750</span>
+          <img class="cardImage " src="/link_black.svg" alt="Linkedin link" />
+        </a>
+
       </div>
+
+
     </section>
   </UIContentArea>
-  <section
-    v-if="copyStatus != ''"
-    style=""
-    class="fixed top-5 text-center transition-all animate-pulse-custom text-white w-full h-10"
-  >
+  <section v-if="copyStatus != ''" style=""
+    class="fixed top-5 text-center transition-all animate-pulse-custom text-white w-full h-10">
     <div class="flex items-center justify-center">
       <div class="bg-main-mark2 p-6 z-20 text-white rounded-md">
         {{ copyStatus }}
@@ -130,6 +75,20 @@ const copyToClipboard = async () => {
     opacity: 0;
   }
 }
+
+.imgPlaceholder {
+  @apply aspect-video w-full block relative rounded-md shadow-lg hover:shadow-2xl hover:scale-105 hover:z-20 transition-all cursor-pointer md:aspect-video;
+  background-size: cover;
+}
+
+.imgPlaceholderIcon {
+  @apply h-[60px] w-[60px] right-[-5%] bottom-[-5%] md:right-[-2%] md:bottom-[-2%] md:h-[80px] md:w-[80px];
+  position: absolute;
+  border-radius: 40px;
+
+}
+
+
 .animate-pulse-custom {
   animation: pulse 2s;
 }
@@ -152,14 +111,16 @@ form div {
 }
 
 .card {
-  @apply flex flex-col justify-center items-center shadow-lg border border-solid border-slate-200 rounded-lg p-4 py-7 gap-3;
+  @apply flex justify-between items-center shadow-lg border border-solid border-slate-200 rounded-lg p-2 md:p-4 md:py-7 gap-3 md:flex-row hover:shadow-2xl hover:scale-105 hover:z-20 transition-all cursor-pointer;
 }
 
 .cardText {
-  @apply flex flex-col text-center gap-2;
+  @apply md:text-2xl md:w-[100%] md:text-left;
 }
 
-.cardText div{
-  @apply flex flex-col ;
+
+
+.cardImage {
+  @apply w-6 aspect-square md:w-[10%];
 }
 </style>
--- components/Projects.vue
+++ components/Projects.vue
@@ -3,9 +3,7 @@
     <section class="w-full flex flex-col gap-4 md:gap-8">
       <h2 class="subtitle">My projects</h2>
       <h1 class="title">My Portfolio</h1>
-      <aside
-        class="grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-4 gap-4 gap-y-5"
-      >
+      <aside class="grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-4 gap-4 gap-y-5">
         <div class="card">
           <img src="/projectsample.png" alt="" />
           <div class="cardText">
@@ -21,31 +19,14 @@
             </div>
           </div>
           <div class="cardButton">
-            <UIButton text="Check the repo" class="group" :icon="true">
-              <svg
-                width="800px"
-                height="800px"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  class="group-hover:stroke-main-mark2"
+            <UIButton text="Check the repo" class="group" :icon="true" size="xs">
+              <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path class="group-hover:stroke-main-mark2"
                   d="M20 4L12 12M20 4V8.5M20 4H15.5M19 12.5V16.8C19 17.9201 19 18.4802 18.782 18.908C18.5903 19.2843 18.2843 19.5903 17.908 19.782C17.4802 20 16.9201 20 15.8 20H7.2C6.0799 20 5.51984 20 5.09202 19.782C4.71569 19.5903 4.40973 19.2843 4.21799 18.908C4 18.4802 4 17.9201 4 16.8V8.2C4 7.0799 4 6.51984 4.21799 6.09202C4.40973 5.71569 4.71569 5.40973 5.09202 5.21799C5.51984 5 6.07989 5 7.2 5H11.5"
-                  stroke="#ffff"
-                  stroke-width="3"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
+                  stroke="#ffff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
             </UIButton>
-            <UIButton
-            class="hidden"
-              text="Check the repo"
-              :color="'clean'"
-              :size="'sm'"
-              :icon="true"
-            >
+            <UIButton class="hidden" text="Check the repo" :color="'clean'" :size="'sm'" :icon="true" size="xs">
               <img src="/github_black.svg" alt="" />
             </UIButton>
           </div>
@@ -65,36 +46,20 @@
             </div>
           </div>
           <div class="cardButton">
-            <UIButton text="Check the repo" class="group" :icon="true">
-              <svg
-                width="800px"
-                height="800px"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  class="group-hover:stroke-main-mark2"
+            <UIButton text="Check the repo" class="group" :icon="true" size="xs">
+              <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path class="group-hover:stroke-main-mark2"
                   d="M20 4L12 12M20 4V8.5M20 4H15.5M19 12.5V16.8C19 17.9201 19 18.4802 18.782 18.908C18.5903 19.2843 18.2843 19.5903 17.908 19.782C17.4802 20 16.9201 20 15.8 20H7.2C6.0799 20 5.51984 20 5.09202 19.782C4.71569 19.5903 4.40973 19.2843 4.21799 18.908C4 18.4802 4 17.9201 4 16.8V8.2C4 7.0799 4 6.51984 4.21799 6.09202C4.40973 5.71569 4.71569 5.40973 5.09202 5.21799C5.51984 5 6.07989 5 7.2 5H11.5"
-                  stroke="#ffff"
-                  stroke-width="3"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
+                  stroke="#ffff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
             </UIButton>
-            <UIButton
-            class="hidden"
-              text="Check the repo"
-              :color="'clean'"
-              :size="'sm'"
-              :icon="true"
-            >
+            <UIButton class="hidden" text="Check the repo" :color="'clean'" :size="'sm'" :icon="true" size="xs">
               <img src="/github_black.svg" alt="" />
             </UIButton>
           </div>
         </div>
-        <div class="hidden md:flex md:flex-col md:gap-7 md:p-4 md:shadow-md md:bg-white md:shadow-stone-300 md:rounded-lg ">
+        <div
+          class="hidden md:flex md:flex-col md:gap-7 md:p-4 md:shadow-md md:bg-white md:shadow-stone-300 md:rounded-lg hover:shadow-2xl hover:scale-105 hover:z-20 transition-all cursor-pointer">
           <img src="/projectsample.png" alt="" />
           <div class="cardText">
             <div class="card2TextTitle">
@@ -109,36 +74,20 @@
             </div>
           </div>
           <div class="cardButton">
-            <UIButton text="Check the repo" class="group" :icon="true" :disabled="true">
-              <svg
-                width="800px"
-                height="800px"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  class="group-hover:stroke-main-mark2"
+            <UIButton text="Check the repo" class="group" :icon="true" size="xs" :disabled="true">
+              <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path class="group-hover:stroke-main-mark2"
                   d="M20 4L12 12M20 4V8.5M20 4H15.5M19 12.5V16.8C19 17.9201 19 18.4802 18.782 18.908C18.5903 19.2843 18.2843 19.5903 17.908 19.782C17.4802 20 16.9201 20 15.8 20H7.2C6.0799 20 5.51984 20 5.09202 19.782C4.71569 19.5903 4.40973 19.2843 4.21799 18.908C4 18.4802 4 17.9201 4 16.8V8.2C4 7.0799 4 6.51984 4.21799 6.09202C4.40973 5.71569 4.71569 5.40973 5.09202 5.21799C5.51984 5 6.07989 5 7.2 5H11.5"
-                  stroke="#ffff"
-                  stroke-width="3"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
+                  stroke="#ffff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
             </UIButton>
-            <UIButton
-            class="hidden"
-              text="Check the repo"
-              :color="'clean'"
-              :size="'sm'"
-              :icon="true"
-            >
+            <UIButton class="hidden" text="Check the repo" :color="'clean'" :size="'sm'" :icon="true" size="xs">
               <img src="/github_black.svg" alt="" />
             </UIButton>
           </div>
-        </div >
-        <div class="hidden 2xl:flex 2xl:flex-col 2xl:gap-7 2xl:p-4 2xl:bg-white 2xl:shadow-md 2xl:shadow-stone-300 2xl:rounded-lg">
+        </div>
+        <div
+          class="hidden 2xl:flex 2xl:flex-col 2xl:gap-7 2xl:p-4 2xl:bg-white 2xl:shadow-md 2xl:shadow-stone-300 2xl:rounded-lg hover:shadow-2xl hover:scale-105 hover:z-20 transition-all cursor-pointer">
           <img src="/projectsample.png" alt="" />
           <div class="cardText">
             <div class="card2TextTitle">
@@ -153,31 +102,14 @@
             </div>
           </div>
           <div class="cardButton">
-            <UIButton text="Check the repo" class="group" :icon="true" :disabled="true">
-              <svg
-                width="800px"
-                height="800px"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  class="group-hover:stroke-main-mark2"
+            <UIButton text="Check the repo" class="group" :icon="true" size="xs" :disabled="true">
+              <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path class="group-hover:stroke-main-mark2"
                   d="M20 4L12 12M20 4V8.5M20 4H15.5M19 12.5V16.8C19 17.9201 19 18.4802 18.782 18.908C18.5903 19.2843 18.2843 19.5903 17.908 19.782C17.4802 20 16.9201 20 15.8 20H7.2C6.0799 20 5.51984 20 5.09202 19.782C4.71569 19.5903 4.40973 19.2843 4.21799 18.908C4 18.4802 4 17.9201 4 16.8V8.2C4 7.0799 4 6.51984 4.21799 6.09202C4.40973 5.71569 4.71569 5.40973 5.09202 5.21799C5.51984 5 6.07989 5 7.2 5H11.5"
-                  stroke="#ffff"
-                  stroke-width="3"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
+                  stroke="#ffff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
             </UIButton>
-            <UIButton
-            class="hidden"
-              text="Check the repo"
-              :color="'clean'"
-              :size="'sm'"
-              :icon="true"
-            >
+            <UIButton class="hidden" text="Check the repo" :color="'clean'" :size="'sm'" :icon="true" size="xs">
               <img src="/github_black.svg" alt="" />
             </UIButton>
           </div>
@@ -205,11 +137,12 @@
 }
 
 .cardTechs div {
-  @apply flex justify-center items-center  bg-main-mark3 px-[1px] py-1 text-white rounded-md w-full text-[15px];
+  @apply flex justify-center items-center bg-main-mark3 px-[1px] py-1 text-white rounded-md w-full text-[10px] md:text-[15px];
 }
 
 .card {
-  @apply flex flex-col gap-7 p-4  bg-white shadow-md shadow-stone-300 rounded-lg;
+  @apply flex flex-col gap-7 p-4 bg-white shadow-md shadow-stone-300 rounded-lg hover:shadow-2xl hover:scale-105 hover:z-20 transition-all cursor-pointer;
+
 }
 
 .cardText {
