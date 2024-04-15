a4e28fe8507f5972d8d5f47f369adce5ced7b142
Vinicius Barbosa Dos Santos <62946143+vini-barbo@users.noreply.github.com>
2024-04-15 12:36:28 -0300

    fix - header space

--- components/HeaderApp.vue
+++ components/HeaderApp.vue
@@ -1,24 +1,24 @@
 <template>
   <div>
     <UIContentArea color="blue">
-      <section class="grid gap-3 md:grid-cols-2 gap-y-8">
-        <div>
-          <span class="flex flex-col gap-3 md:gap-9 md:h-full md:justify-center 2xl:gap-14 w-full">
-            <h2>{{ $t('welcome') }}</h2>
-            <h1><mark>Vinicius Barbosa</mark></h1>
-            <h1>{{ $t('role') }}</h1>
-            <h2 class="w-max">
-              {{ $t('headerSubtitle1') }} <mark>{{ $t('headerSubtitle2') }}</mark> {{ $t('headerSubtitle3')
-              }}<mark>&nbsp{{
+      <section class="grid gap-3 md:grid-cols-2 gap-y-8 md:w-full">
+
+        <div class="flex flex-col gap-3 md:gap-9 md:h-full md:justify-center 2xl:gap-14  md:w-full">
+          <h2>{{ $t('welcome') }}</h2>
+          <h1><mark>Vinicius Barbosa </mark></h1>
+          <h1 class="md:w-full">{{ $t('role') }}</h1>
+          <h2>
+            {{ $t('headerSubtitle1') }} <mark>{{ $t('headerSubtitle2') }}</mark> {{ $t('headerSubtitle3')
+            }}<mark>&nbsp{{
   $t('headerSubtitle4')
 }}</mark>
-            </h2>
-            <a href="/curriculum_Vinicius_Barbosa.pdf" download>
-              <UIButton :text="$t('resume')" />
-            </a>
-          </span>
+          </h2>
+          <a href="/curriculum_Vinicius_Barbosa.pdf" download>
+            <UIButton :text="$t('resume')" />
+          </a>
         </div>
-        <div class="2xl:max-h-[40vw] 2xl:aspect-square md:p-14">
+
+        <div class="2xl:max-h-[40vw] 2xl:aspect-square md:p-14 ">
           <img src="/myphotoedited.png" alt="A professional photo of vinicius barbosa" />
         </div>
       </section>
